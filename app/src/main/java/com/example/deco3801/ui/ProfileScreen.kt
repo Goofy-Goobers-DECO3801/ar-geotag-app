340f4dc77ccf13b69fc42c721a8761ee916e49d7
Lucy Ochre <lucyochre@d-i89-133-100.student.eduroam.uq.edu.au>
2023-08-30 11:58:53 +1000

    initial commit for profile-framework

--- app/src/main/java/com/example/deco3801/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/deco3801/ui/ProfileScreen.kt
@@ -1,19 +1,27 @@
 package com.example.deco3801.ui
 
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.Image
 import com.example.deco3801.ui.components.NavBar
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.deco3801.R
 
 @Composable
 fun ProfileScreen() {
@@ -22,10 +30,36 @@ fun ProfileScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Profile Screen",
-            style = MaterialTheme.typography.headlineLarge
+        Image(
+            painter = painterResource(id = R.drawable.pfp),
+            contentDescription = "profile",
+            modifier = Modifier.size(62.dp)
         )
+        Text(
+            text = "Full Name",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = "@Username",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Edit Profile")
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "5 posts"
+            )
+            Text(
+                text = "12 followers"
+            )
+            Text(
+                text = "13 following"
+            )
+        }
     }
 }
 
@@ -34,3 +68,42 @@ fun ProfileScreen() {
 fun ProfileScreenPreview() {
     ProfileScreen()
 }
+
+@Composable
+fun ArtworkTile(
+    artworkTitle: String,
+    date: String,
+    location: String,
+    likes: Int,
+    reviews: Int,
+    img: Drawable
+    ) {
+
+    Image(
+        painter = painterResource(id = img),
+        contentDescription = "post",
+        modifier = Modifier.size(62.dp)
+    )
+    Text(
+        text = artworkTitle,
+        style = MaterialTheme.typography.titleMedium
+    )
+    Text(
+        text = "Created on $date"
+    )
+    Text(
+        text = location
+    )
+    Text(
+        text = "" + likes + "likes"
+    )
+    Text(
+        text = "" + reviews + "reviews"
+    )
+
+
+
+
+}
+
+
