7fadb159714af88895c9684aab5ee3a1cd00f83c
Damian Bellew <damian.bellew@gmail.com>
2023-09-02 21:01:47 +1000

    Fixed issue with UserInput. Also hides password, and unhides when pressed

--- app/src/main/java/com/example/deco3801/Login.kt
+++ app/src/main/java/com/example/deco3801/Login.kt
@@ -10,8 +10,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,42 +30,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.deco3801.ui.theme.MyColors
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UserInput (inputName: String, initialValue: String) {
-    var inputValue by remember { mutableStateOf(initialValue) }
-
-    Column (
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 30.dp,
-                end = 30.dp,
-                top = 10.dp,
-                bottom = 10.dp)
-    ) {
-        Text(
-            text = inputName,
-            style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-            color = Color.White
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        TextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = inputValue,
-            onValueChange = {newValue -> inputValue = newValue},
-        )
-    }
-}
-@Composable
 fun LoginScreen(navController: NavHostController) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
 
     Column (
         modifier = Modifier
@@ -74,8 +58,39 @@ fun LoginScreen(navController: NavHostController) {
             contentDescription = "Logo",
             modifier = Modifier.size(150.dp)
         )
-        UserInput(inputName = "Username or email", initialValue = username)
-        UserInput(inputName = "Password", initialValue = password)
+
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp,
+                    end = 30.dp,
+                    top = 30.dp,
+                    bottom = 10.dp),
+            value = username,
+            onValueChange = {newUsername -> username = newUsername},
+            label = { Text("Username or email") }
+        )
+
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp,
+                    end = 30.dp,
+                    top = 10.dp,
+                    bottom = 20.dp),
+            value = password,
+            onValueChange = {newPassword -> password = newPassword},
+            label = { Text("Password") },
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(
+                    onClick = { isPasswordVisible = !isPasswordVisible }
+                ) {
+                    val icon = if (isPasswordVisible) Icons.Default.Check else Icons.Default.Lock
+                    Icon(imageVector = icon, contentDescription = null)
+                }
+            }
+        )
 
         Spacer(modifier = Modifier.height(20.dp))
 
@@ -90,4 +105,10 @@ fun LoginScreen(navController: NavHostController) {
             Text("Don't have an account? Sign Up")
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewLoginScreen() {
+    LoginScreen(navController = rememberNavController())
 }
--- app/src/main/java/com/example/deco3801/SignUp.kt
+++ app/src/main/java/com/example/deco3801/SignUp.kt
@@ -10,8 +10,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,16 +30,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.deco3801.ui.theme.MyColors
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpScreen(navController: NavHostController) {
     var username by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
 
     Column (
         modifier = Modifier
@@ -48,14 +57,63 @@ fun SignUpScreen(navController: NavHostController) {
             contentDescription = "Logo",
             modifier = Modifier.size(150.dp)
         )
-        UserInput(inputName = "Username", initialValue = username)
-        UserInput(inputName = "Email", initialValue = email)
-        UserInput(inputName = "Password", initialValue = password)
+
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp,
+                    end = 30.dp,
+                    top = 30.dp,
+                    bottom = 10.dp),
+            value = username,
+            onValueChange = {newUsername -> username = newUsername},
+            label = { Text("Username") }
+        )
+
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp,
+                    end = 30.dp,
+                    top = 10.dp,
+                    bottom = 10.dp),
+            value = email,
+            onValueChange = {newEmail -> email = newEmail},
+            label = { Text("Email") }
+        )
+
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp,
+                    end = 30.dp,
+                    top = 10.dp,
+                    bottom = 20.dp),
+            value = password,
+            onValueChange = {newPassword -> password = newPassword},
+            label = { Text("Password") },
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(
+                    onClick = { isPasswordVisible = !isPasswordVisible }
+                ) {
+                    val icon = if (isPasswordVisible) Icons.Default.Check else Icons.Default.Lock
+                    Icon(imageVector = icon, contentDescription = null)
+                }
+            }
+        )
 
         Spacer(modifier = Modifier.height(20.dp))
 
         Button(onClick = { navController.navigate(ScreenNames.Screen.name) }) {
             Text("Sign Up", fontSize = 23.sp)
+        }
+
+        Button(
+            onClick = { navController.navigate(ScreenNames.Login.name) },
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text("Already have an account? Log in")
         }
     }
 }
