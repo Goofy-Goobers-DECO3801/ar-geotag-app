c35dc15d81c35b9bc4427079603bbebe2545e589
Damian Bellew <damian.bellew@gmail.com>
2023-09-14 14:42:35 +1000

    Fixed issue of lost terms and conditions checkbox on merge

--- app/src/main/java/com/example/deco3801/ui/SignUpScreen.kt
+++ app/src/main/java/com/example/deco3801/ui/SignUpScreen.kt
@@ -5,21 +5,31 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -39,6 +49,10 @@ fun SignUpScreen(
 ) {
     val uiState = viewModel.uiState
     val context = LocalContext.current
+    var isChecked by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf<String?>(null) }
+    var viewTandC by remember { mutableStateOf(false) }
+    var viewPrivacyPolicy by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -94,7 +108,43 @@ fun SignUpScreen(
             onValueChange = viewModel::onPasswordChange
         )
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            modifier = Modifier.padding(
+                start = 30.dp,
+                end = 30.dp,
+                top = 0.dp,
+                bottom = 20.dp
+            ),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = isChecked,
+                onCheckedChange = { isChecked = it }
+            )
+            val agreementText = "I agree to Terms and Conditions and Privacy Policy"
+            ClickableText(
+                text = AnnotatedString(agreementText),
+                onClick = { offset ->
+                    if (offset >= agreementText.indexOf("Terms and Conditions") &&
+                        offset < agreementText.indexOf("Terms and Conditions") + "Terms and Conditions".length) {
+                        viewTandC = true
+                    } else if (offset >= agreementText.indexOf("Privacy Policy") &&
+                        offset < agreementText.indexOf("Privacy Policy") + "Privacy Policy".length) {
+                        viewPrivacyPolicy = true
+                    }
+                },
+                style = TextStyle(fontSize = 15.sp, color = Color.White)
+            )
+        }
+
+        if (viewTandC) {
+            TandCDialog { viewTandC = false }
+        }
+
+        if (viewPrivacyPolicy) {
+            PrivacyPolicyDialog { viewPrivacyPolicy = false }
+        }
 
         Button(onClick = {
             viewModel.onSignUpClicked(
