07d0f91b6cc2af6116e8de3288cfb73bee466b50
Damian Bellew <damian.bellew@gmail.com>
2023-09-14 18:50:00 +1000

    Changed password field visibility on/off icons

--- app/src/main/java/com/example/deco3801/ui/components/TextField.kt
+++ app/src/main/java/com/example/deco3801/ui/components/TextField.kt
@@ -1,9 +1,12 @@
 package com.example.deco3801.ui.components
 
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,9 +18,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import com.example.deco3801.R
+import androidx.compose.ui.res.vectorResource
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -39,7 +46,7 @@ fun PasswordField(
             IconButton(
                 onClick = { isPasswordVisible = !isPasswordVisible }
             ) {
-                val icon = if (isPasswordVisible) Icons.Default.Check else Icons.Default.Lock
+                val icon = if (isPasswordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
                 Icon(imageVector = icon, contentDescription = null)
             }
         },
