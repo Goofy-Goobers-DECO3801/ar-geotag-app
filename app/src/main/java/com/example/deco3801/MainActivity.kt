b853def3aeca6917f78f5a9fab6be0c08f8c66cd
TrumpetFlotty <carl_flottmann@optusnet.com.au>
2023-09-29 17:40:15 +1000

    Added python start check in MainActivity.kt

--- app/src/main/java/com/example/deco3801/MainActivity.kt
+++ app/src/main/java/com/example/deco3801/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
 import com.example.deco3801.artdisplay.presentation.ArtDisplayViewModel
 import com.example.deco3801.ui.theme.DECO3801Theme
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,7 +28,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // TODO("look into dependency injection")
+
         val virtualTryOnViewModel by viewModels<ArtDisplayViewModel>()
+
+        if (! Python.isStarted()) {
+            Python.start(AndroidPlatform(this));
+        }
 
         super.onCreate(savedInstanceState)
 
